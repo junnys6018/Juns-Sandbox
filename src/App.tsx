bca6a7fb4b87ecb97a883c1b0daae691f0a98378
junnys6018 <junkanglim1@gmail.com>
2022-01-21 14:34:05 +0800

    update <p> tag

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,7 @@ function App() {
             <header className="App-header">
                 <img src={logo} className="App-logo" alt="logo" />
                 <p className="text-green-600">
-                    Edit <code>src/App.tsx</code> and save to reload.
+                    Hello, World!
                 </p>
                 <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
                     Learn React
