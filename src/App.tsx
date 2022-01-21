504cceee80e2c8436de3fb4cd93c022ef4f871bf
junnys6018 <junkanglim1@gmail.com>
2022-01-21 15:51:19 +0800

    Update <p> tag

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,7 @@ function App() {
             <header className="App-header">
                 <img src={logo} className="App-logo" alt="logo" />
                 <p className="text-green-600">
-                    Hello, World!
+                    Hello, World! This is the staging site
                 </p>
                 <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
                     Learn React
