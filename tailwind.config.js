81c6b69f2e34610cc8f43f0e792680c03c2220dd
junnys6018 <junkanglim1@gmail.com>
2022-01-21 23:43:11 +0800

    added routing and set custom favicon

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,31 @@
 module.exports = {
   content: [
     "./src/**/*.{js,jsx,ts,tsx}",
+    "./public/index.html",
   ],
   theme: {
+    fontFamily: {
+      sans: ['Poppins', 'system-ui']
+    },
+    screens: {
+      xs: '375px',
+      sm: '576px',
+      md: '768px',
+      lg: '992px',
+      xl: '1200px',
+      '2xl': '1400px',
+    },
+    container: {
+      center: true,
+      padding: {
+        DEFAULT: '1rem',
+        sm: '18px',
+        md: '24px',
+        lg: '16px',
+        xl: '30px',
+        '2xl': '40px',
+      },
+    },
     extend: {},
   },
   plugins: [],
