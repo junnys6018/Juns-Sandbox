85794ebcb2ecee89ff58e847466ad4df8ed076c4
junnys6018 <junkanglim1@gmail.com>
2022-03-06 18:29:42 +0800

    implemented 404 page

--- src/pages/not-found/not-found.test.tsx
+++ src/pages/not-found/not-found.test.tsx
@@ -1,7 +1,8 @@
 import { render } from '@testing-library/react';
+import { MemoryRouter } from 'react-router-dom';
 import NotFound from './not-found';
 
 test('<NotFound /> snapshot', () => {
-    const { asFragment } = render(<NotFound />);
+    const { asFragment } = render(<NotFound />, { wrapper: MemoryRouter });
     expect(asFragment()).toMatchSnapshot();
 });
--- src/pages/not-found/not-found.tsx
+++ src/pages/not-found/not-found.tsx
@@ -1,3 +1,18 @@
+import Footer from 'components/footer';
+import Navbar from 'components/navbar';
+import Socials from 'components/socials';
+
 export default function NotFound() {
-    return <p>404 page</p>;
+    return (
+        <div className="flex flex-col min-h-screen">
+            <Navbar />
+            <div className="text-center mt-[20vh]">
+                <p className="font-semibold text-pink-600 text-6xl md:text-7xl mb-8 md:mb-11">Oops</p>
+                <p className="font-medium text-pink-600 text-3xl md:text-4xl mb-4 md:mb-5">Something Went Wrong</p>
+                <p className="font-normal text-neutral-800 text-xl md:text-2xl">Error 404 Page Not Found</p>
+            </div>
+            <Footer />
+            <Socials />
+        </div>
+    );
 }
